b4e53cb0bd55267f8813a9284fd4e06c9628873d
Fasca-kcct <r121317@g.kobe-kosen.ac.jp>
2022-09-05 23:50:52 +0900

    フッターの調整

--- libs/src/components/molecules/Header/index.tsx
+++ libs/src/components/molecules/Header/index.tsx
@@ -1,13 +1,19 @@
 import { FC } from "react"
+import { css } from "@emotion/css"
 
 //タイトルのアイコンでクリックするとページ遷移したい
 const Header: FC = () => {
   return (
     <header>
-      <h1>ooなう</h1>
+      <h1 className={styles.h1}>ooなう</h1>
       
     </header>
   )
 }
 
+const styles = {
+  h1: css`
+    text-align: center;
+  `
+}
 export default Header
