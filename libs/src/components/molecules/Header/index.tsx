bf037f519e6e16dba9ac50cd4662bf63ca22c5c5
Fasca <r121317@g.kobe-kosen.ac.jp>
2022-09-16 18:50:12 +0900

    add menu

--- libs/src/components/molecules/Header/index.tsx
+++ libs/src/components/molecules/Header/index.tsx
@@ -1,21 +1,36 @@
 import { FC } from "react"
 import { css } from "@emotion/react"
 import Logo from "components/atoms/Logo"
+import Menu from "components/molecules/Menu"
 
-//タイトルのアイコンでクリックするとページ遷移したい
 const Header: FC = () => {
   return (
     <header css={styles.title}>
-      <Logo />
+      <div css={styles.menu}>
+      <Menu title="Menu">
+        <Menu title="Sign Up"></Menu>
+        <Menu title="Login"></Menu>
+        <Menu title="Profile"></Menu>
+      </Menu>
+      </div>
+      <div css={styles.logo}>
+        <Logo />
+      </div>
+      <div/>
     </header>
   )
 }
 
 const styles = {
   title: css`
-    text-align: center;
     padding-top: 0px;
     margin-top: 0px;
+    display: flex;
+    justify-content: space-between;
+  `,
+  logo: css`
+    display: block;
   `
+  
 }
 export default Header
