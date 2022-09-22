df014b93a9a48369904727826d5884352b909db3
YamaD <me@dyama.net>
2022-09-23 08:59:58 +0900

    comments

--- server/src/auth/constants.ts
+++ server/src/auth/constants.ts
@@ -1,3 +1,4 @@
 export const jwtConstants = {
+  // 動かない場合はREADME参照
   secret: process.env.JWT_SECRET,
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,14 +3,30 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify"
+import secureSession from "@fastify/secure-session"
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
 import { AppModule } from "app.module"
+
+if (!process.env.COOKIE_SECRET) {
+  throw new Error("COOKIE_SECRET is not set !")
+}
+
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET is not set !")
+}
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   )
+  await app.register(secureSession, {
+    key: Buffer.from(process.env.COOKIE_SECRET!, "hex"),
+    cookie: {
+      path: "/",
+      httpOnly: true,
+    },
+  })
 
   const config = new DocumentBuilder()
     .setTitle("ooNow API")
