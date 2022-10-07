76548d0d3bf7f6d73374d9a0e662a1c943a85e8a
kentos731 <r119320@g.kobe-kosen.ac.jp>
2022-10-07 15:46:12 +0900

    post

---
+++ libs/src/components/organisms/PostForm/index.tsx
@@ -0,0 +1,62 @@
+import React from "react"
+import { css } from "@emotion/react"
+
+import UIButton from "components/atoms/UIButton"
+import NowButton from "components/molecules/NowButton"
+import Choice from "components/organisms/Choice"
+
+const Post = () => {
+  return (
+    <>
+      <div
+        css={css`
+                justify-content: center;
+                display: flex;
+                margin-bottom: 20px
+            `}>
+        <NowButton text='遊び' />
+        <NowButton text='食事' />
+        <NowButton text='睡眠' />
+        <NowButton text='勉強・仕事' />
+      </div>
+      <div
+        css={css`
+                display: flex;
+                justify-content: center;
+                margin-bottom: 50px;
+                
+            `}>
+        <form action=''>
+          <input
+            css={css`
+                    border: medium solid black;
+                    border-radius: 1em;
+                    resize: none;
+                `}
+            name=''
+            id=''
+            cols='20'
+            rows='5'
+            autoFocus='true'
+            placeholder='いましていること'></input>
+        </form>
+        <div
+          css={css`
+                    margin-top: 80px;
+                    margin-left: 40px;
+                `}></div>
+        <div
+          css={css`
+                display: flex;
+                justify-content: center;
+                margin-bottom: 50px;
+                
+            `}>
+          <UIButton text='なう' strong />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Post
