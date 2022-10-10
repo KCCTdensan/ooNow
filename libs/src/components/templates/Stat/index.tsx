fe1c3328f91b3092f754e163be520a6e302dbe84
Fasca-kcct <r121317@g.kobe-kosen.ac.jp>
2022-10-10 20:55:11 +0900

    fixed

--- libs/src/components/templates/Stat/index.tsx
+++ libs/src/components/templates/Stat/index.tsx
@@ -24,19 +24,42 @@ const Stat = () => {
 	        <h1>〇〇なう For Company</h1>
 	        <h3>当サービスを利用しているユーザーの統計情報を表示します。</h3>
   		</div>
-
-		  <NowShow size={100} name="Sample" nowValue={50} wasValue={25} age={30} gender="男" color="aquamarine"/>
-
-		 <div css={
-			css`
-				width: 100%;
-			`
-		} className="map">
-
-			<NowShow size={150} name="Game" nowValue={100} wasValue={50} age={10} gender="男" color="#0051ff" />
-			<NowShow size={120} name="study" nowValue={40} wasValue={60} age={10} gender="女" color="#00d836" />
-			<NowShow size={212} name="job" nowValue={120} wasValue={60} age={30} gender="男" color="#ffae00" />
-		</div>
+			<div css={
+				css`
+					position:relative;
+					top: 10px;
+					left: 10px;
+				`
+			}>
+				<NowShow size={100} name="Sample" nowValue={50} wasValue={25} age={30} gender="男" color="aquamarine"/>
+			</div>
+			<div css={
+				css`
+					position:relative;
+					top: -60px;
+					left: 130px;
+				`
+			}>
+				<NowShow size={212} name="Game" nowValue={100} wasValue={50} age={10} gender="男" color="#0051ff" />
+			</div>
+			<div css={
+				css`
+					position:relative;
+					top: -55px;
+					left: 30px;
+				`
+			}>
+				<NowShow size={212} name="study" nowValue={40} wasValue={60} age={10} gender="女" color="#00d836" />
+			</div>
+			<div css={
+				css`
+					position:relative;
+					top: -340px;
+					left: 210px;
+				`
+			}>
+				<NowShow size={173} name="job" nowValue={120} wasValue={60} age={30} gender="男" color="#ffae00" />
+			</div>
 		</div>
 		<Footer />
 		</>
