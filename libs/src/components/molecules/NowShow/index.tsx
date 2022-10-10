0b113aa5e2d7db719eda3612d320a5cc6c5f3db1
Fasca-kcct <r121317@g.kobe-kosen.ac.jp>
2022-10-10 17:43:33 +0900

    add stat page

---
+++ libs/src/components/molecules/NowShow/index.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { css } from "@emotion/react"
+
+type nowProps = {
+    size: number,
+    nowValue: number,
+    wasValue: number,
+    name: string,
+    age: number,
+    gender: string,
+    color: any,
+}
+
+const NowShow: React.FC<nowProps> = ({size,nowValue,name,wasValue,age,gender,color}) => {
+    const [checkFlag, setCheckFlag] = useState(false);
+    return (
+        <>
+        <div css={
+			css`
+				/*仮、実際にはカテゴリの投稿数に比例したpx数 */
+				background-color: ${color}; /* 仮、実際には各カテゴリの色が入る */
+				width: ${size}px;
+				height: ${size}px; 
+				border-radius: 50%;
+				text-align: center;
+				line-height: ${size}px;
+			`
+		} className="nows">
+	        <label css={
+				css`
+					cursor: pointer;
+				`
+			} className="category_name_button" htmlFor="category_detail_status">{name}</label>
+	        <input css={
+				css`
+					display: none;
+					&:checked{
+						display: none;
+						background-color: #00000070;;
+						width: 100vw;
+						height: 100vh;
+						z-index: 9999;
+						position: fixed;
+						top: 0;
+						left: 0;
+					}
+				`
+			} type="checkbox" id="category_detail_status" checked={checkFlag} onChange={(e) => setCheckFlag(!checkFlag)}/>
+	        <div css={
+				css`
+					display: ${checkFlag ? "block" : "none"};
+					background-color: #00000070;;
+					width: 100vw;
+					height: 100vh;
+					z-index: 9999;
+					position: fixed;
+					top: 0;
+					left: 0;
+				`
+			} className="overlay">
+	            <div css={
+					css`
+						display: block;
+						width: 470px;
+						height: 260px;
+
+						background-color: rgb(243, 243, 243);
+						border-radius: 1%;
+						text-align: center;
+						margin-left: 15vw;
+						margin-top: 25vh;
+						margin-bottom: 10vh;
+
+						font-weight:500;
+						font-size: large;
+						line-height:normal;
+					`
+				} className="category_detail">
+	                <div css={
+						css`
+							margin: 5%;
+						`
+					} className="text">現在有効な総”なう”数：{nowValue.toString()}</div>
+	                <div css={
+						css`
+							margin: 5%;
+						`
+					} className="text">カテゴリー：{name}</div>
+	                <div css={
+						css`
+							margin: 5%;
+						`
+					} className="text">累積総”わず”数：{wasValue.toString()}</div>
+	                <div css={
+						css`
+							margin: 5%;
+						`
+					} className="text">次のユーザーに人気：{age.toString()}代 {gender}性</div>
+	                <div css={
+						css`
+							margin: 5%;
+						`
+					} className="text">
+	                <label css={
+						css`
+							cursor: pointer;
+            				height:20%;
+						`
+					} className="close_button" htmlFor="category_detail_status">[閉じる]</label>
+	                </div>
+	            </div>
+	        </div>
+	    </div> 
+        </>
+
+    )
+}
+
+export default NowShow
---
+++ libs/src/components/templates/Stat/index.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import { css } from "@emotion/react"
+import NowShow from "components/molecules/NowShow"
+import Header from "components/organisms/Header"
+import Footer from "components/organisms/Footer"
+
+const Stat = () => {
+	return (
+		<>
+		<Header />
+		<div css={
+			css`
+				width:70%;
+            	margin-left:15%;
+            	margin-right:15%;
+			`
+		}>
+		<div css={
+			css`
+				height:20%;
+			`
+		} 
+		className="info">
+	        <h1>〇〇なう htmlFor Company</h1>
+	        <h3>当サービスを利用しているユーザーの統計情報を表示します。</h3>
+  		</div>
+
+		  <NowShow size={100} name="Sample" nowValue={50} wasValue={25} age={30} gender="男" color="aquamarine"/>
+
+		 <div css={
+			css`
+				width: 100%;
+			`
+		} className="map">
+
+			<NowShow size={150} name="Game" nowValue={100} wasValue={50} age={10} gender="男" color="#0051ff" />
+			<NowShow size={120} name="study" nowValue={40} wasValue={60} age={10} gender="女" color="#00d836" />
+			<NowShow size={212} name="job" nowValue={120} wasValue={60} age={30} gender="男" color="#ffae00" />
+		</div>
+		</div>
+		<Footer />
+		</>
+	)
+} 
+
+export default Stat
