8302f6198b7ecc923e27d8405013bf412331975d
jnc-explosion <himajin8634@gmail.com>
2022-10-14 22:29:19 +0900

    wipu wip

---
+++ libs/src/components/organisms/TimeLine/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react"
+import { css } from "@emotion-react"
+
+const TimeLine = () => {
+	return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default TimeLine
