813c5462d7f54959accc6292718584ceafbe9024
tai-cyan <taisukemar@icloud.com>
2022-10-16 08:34:08 +0900

    home -> post

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,19 @@
 import { HomeTemplate } from "@kcctdensan/oonow-libs/next"
+import { ButtonLink } from "@kcctdensan/oonow-libs/next"
+import { css } from "@emotion/react"
 
 export default function IndexScreen() {
   return (
-    <HomeTemplate />
+    <>
+      <div
+        css={css`
+          position: fixed;
+          bottom: 3rem;
+          right: 3rem;
+          `}>
+        <ButtonLink text='投稿' to='/post' strong={true} />
+      </div>
+      <HomeTemplate />
+    </>
   )
 }
